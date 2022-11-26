8c3564569b6e10c78f1cade69baaa61b0ce3cf47
Andrei Micu <andrei.micu@visma.com>
2022-11-26 23:22:16 +0200

    Add "checkbox" option to know if the person that filled in the form needs help or helps someone instead. Add login system and endpoint.

--- src/main/java/com/timishelter/timishelter/repository/UserRepository.java
+++ src/main/java/com/timishelter/timishelter/repository/UserRepository.java
@@ -6,10 +6,16 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository("user_application")
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     @Query( value = "SELECT COUNT(*) FROM user_application ua WHERE ua.username LIKE %:username%",
-    nativeQuery = true)
-    int getCountOfSameUsernameUsers( String username);
+            nativeQuery = true)
+    int getCountOfSameUsernameUsers(String username);
+
+    @Query( value = "SELECT * FROM user_application ua WHERE ua.username = :username LIMIT 1",
+            nativeQuery = true)
+    Optional<User> getUserByUsername(String username);
 }
