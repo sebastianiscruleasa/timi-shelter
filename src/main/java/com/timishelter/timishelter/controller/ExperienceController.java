049fc3486879e5c4f9befdc0e5c4f95807cf5a76
Caracoancea Timotei <caracoancea.timotei@gmail.com>
2022-11-26 17:29:34 +0200

    Added experience.

---
+++ src/main/java/com/timishelter/timishelter/controller/ExperienceController.java
@@ -0,0 +1,38 @@
+package com.timishelter.timishelter.controller;
+
+import com.timishelter.timishelter.model.Experience;
+import com.timishelter.timishelter.repository.ExperienceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+@RequestMapping(path="/experience")
+public class ExperienceController {
+
+    @Autowired
+    private ExperienceRepository experienceRepository;
+
+    @PostMapping("/add")
+    @ResponseBody
+    public String addExperience(
+            @RequestParam String username,
+            @RequestParam String description
+    ){
+        Experience e= new Experience();
+        e.setUsername(username);
+        e.setDescription(description);
+        experienceRepository.save(e);
+        return "Saved";
+    }
+
+    @GetMapping("/get")
+    public Optional<Experience> getExperience(
+            @RequestParam String username
+    ){
+        return experienceRepository.findById(username);
+    }
+
+}
---
+++ src/main/java/com/timishelter/timishelter/repository/ExperienceRepository.java
@@ -0,0 +1,7 @@
+package com.timishelter.timishelter.repository;
+
+import com.timishelter.timishelter.model.Experience;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ExperienceRepository extends CrudRepository<Experience,String> {
+}
